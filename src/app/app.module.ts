8474148daa64ea43b5dec151b82ea146cb769daf
Enotfttp <62541039+Enotfttp@users.noreply.github.com>
2021-07-13 22:25:31 +0300

    Create chat Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatButtonModule } from "@angular/material";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatGridListModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    // MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
