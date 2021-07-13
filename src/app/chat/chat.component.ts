29662d8831348fc78435722efbc62c0fe22267bb
dima.lepahin <dima.lepahin@mail.ru>
2021-07-13 23:06:20 +0300

    Shows video from the camera

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef,  ViewChild} from '@angular/core';
+
+const mediaConstrains = {
+  audio: true,
+  video: {width: 1920, height: 1080}
+};
+
+
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements AfterViewInit {
 
+  private localStream: MediaStream;
+  @ViewChild('local_video') localVideo: ElementRef;
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.requestMediaDevices();
   }
 
+
+  private async requestMediaDevices(): Promise<void> {
+    this.localStream = await navigator.mediaDevices.getUserMedia(mediaConstrains);
+    this.localVideo.nativeElement.srcObject = this.localStream;
+  }
 }
